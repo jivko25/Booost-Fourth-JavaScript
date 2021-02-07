33c916b72563a57eb0fa21b461080d681ab33955
jivko25 <jivkou@yahoo.com>
2021-02-07 09:39:58 +0200

    changes

---
+++ src/app/custom/StarWarsUniverse.js
@@ -0,0 +1,52 @@
+import EventEmitter from 'eventemitter3';
+import Species from './Species';
+
+
+const EVENTS = {
+    MAX_SPECIES_REACHED : 'max_species_reached', 
+    SPECIES_CREATED: 'species_created',
+  };
+
+export default class StarWarsUniverse extends EventEmitter{
+    constructor(){
+        super();
+
+        this.species = [];
+        this._maxSpecies = 10;
+    }
+
+    static get events(){
+        return EVENTS;
+    }
+
+    get speciesCount(){
+        var arr = this.species;
+        return arr.length;
+    }
+
+    async init(){
+        const spec = new Species();
+        // var i = 1;
+        var arr = [];
+        this.on(StarWarsUniverse.events.SPECIES_CREATED, async function(){
+                arr = this.species;
+                var url = 'https://swapi.booost.bg/api/species/';
+                // console.log(await spec.init(url, i));
+                const res = await spec.init(url, i);
+                arr.push(res);
+                this.species = arr;
+                localStorage.setItem('size', i)
+        });
+        for(var i = 1;i<=this._maxSpecies;i++){
+        this.emit(StarWarsUniverse.events.SPECIES_CREATED)
+        }
+        this.emit(StarWarsUniverse.events.MAX_SPECIES_REACHED)
+        // }
+        // this.emit(StarWarsUniverse.events.SPECIES_CREATED)
+        // i++;
+        // this.emit(StarWarsUniverse.events.SPECIES_CREATED)
+        // this.species = arr;
+        // this.speciesCount;
+        console.log(this.species)
+    }
+}
